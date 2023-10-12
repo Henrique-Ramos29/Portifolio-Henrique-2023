e8189ce018f1eeec22f8113d3fe0cdb425f6ef2d
Henrique-Ramos29 <120992364+Henrique-Ramos29@users.noreply.github.com>
2023-10-12 16:50:18 -0300

    commit icone rede socias

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,7 +3,7 @@ import Foto from '../../../public/imagem/henrique.jpg';
 import BotaoPrincipal from '../BotaoPrincipal';
 import {motion} from 'framer-motion';
 import { AiFillGithub, AiFillLinkedin, AiFillInstagram } from "react-icons/ai";
-
+import Typical from 'react-typical'
 
 const Menu = () => {
     
@@ -15,16 +15,30 @@ const Menu = () => {
             initial={{ y: -350 }} 
             animate={{ y: -10, scale: 1.7}}
             className="Imagem_Henrique" src={Foto} width={150} height={150} />
-            <motion.h1 className='Titulo' >Olá meu nome é Henrique Ramos</motion.h1>
-            <motion.h2 className='Subtitulo' >Developer Front-End</motion.h2>
+            <motion.h1 className='Titulo' >Hi, I'am Henrique {''} 
+              <Typical loop={Infinity}
+                       
+                       steps={[
+                        'Developer',
+                        1000,
+                        'Front-End',
+                        1000,
+                    ]} />
+            </motion.h1>     
         <motion.div animate={{ scale: 1.0 }} className='Div_Botao' >
             <BotaoPrincipal>Curriculo</BotaoPrincipal>
             <BotaoPrincipal>Contato</BotaoPrincipal>
         </motion.div>
             <motion.div className='containerRedeSocial' >
-                <AiFillGithub size={35}/>
-                <AiFillLinkedin size={35} />
-                <AiFillInstagram size={35   } />
+                    <a className='icone' href="https://github.com/Henrique-Ramos29/">
+                      <AiFillGithub size={30}/>
+                    </a>
+                    <a className='icone' href="https://www.linkedin.com/in/henrique-ramos2023/">
+                      <AiFillLinkedin size={30} />
+                    </a>
+                    <a className='icone' href="https://www.instagram.com/henrique_ramos27/?igshid=Mzc0YWU1OWY=">
+                      <AiFillInstagram size={30} />
+                    </a>
             </motion.div>
         </motion.div>
         </>
